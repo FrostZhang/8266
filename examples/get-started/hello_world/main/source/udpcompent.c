a0211cacd81741a4e8573de27494ef0762c74c1e
Frostzhang <376237519@qq.com>
2021-05-28 12:20:06 +0800

    udp bug

--- examples/get-started/hello_world/main/source/udpcompent.c
+++ examples/get-started/hello_world/main/source/udpcompent.c
@@ -13,6 +13,7 @@ int sock = -1;
 struct sockaddr_in sendAddr = {0};
 udp_callback_t callback;
 static TaskHandle_t handel;
+bool state;
 extern void udp_client_bord(const char *data)
 {
         if (sock < 0)
@@ -45,6 +46,16 @@ extern void udp_client_sendto(in_addr_t addr, const char *data)
                 ESP_LOGE(TAG, "Error occured during sending: errno %d", errno);
 }
 
+// #else // IPV6
+//                 struct sockaddr_in6 sendAddr;
+//                 inet6_aton(HOST_IP_ADDR, &sendAddr.sin6_addr);
+//                 sendAddr.sin6_family = AF_INET6;
+//                 sendAddr.sin6_port = htons(PORT);
+//                 addr_family = AF_INET6;
+//                 ip_protocol = IPPROTO_IPV6;
+//                 inet6_ntoa_r(sendAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+// #endif
+
 static void udp_client_task(void *pvParameters)
 {
         char rx_buffer[256];
@@ -52,25 +63,15 @@ static void udp_client_task(void *pvParameters)
         int addr_family;
         int ip_protocol;
         udp_event event_t;
-        while (1)
+        while (state)
         {
-
-#ifdef CONFIG_EXAMPLE_IPV4
                 sendAddr.sin_addr.s_addr = -1;
                 sendAddr.sin_family = AF_INET;
                 sendAddr.sin_port = htons(PORT);
                 addr_family = AF_INET;
                 ip_protocol = IPPROTO_IP;
                 inet_ntoa_r(sendAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
-#else // IPV6
-                struct sockaddr_in6 sendAddr;
-                inet6_aton(HOST_IP_ADDR, &sendAddr.sin6_addr);
-                sendAddr.sin6_family = AF_INET6;
-                sendAddr.sin6_port = htons(PORT);
-                addr_family = AF_INET6;
-                ip_protocol = IPPROTO_IPV6;
-                inet6_ntoa_r(sendAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
-#endif
+
                 sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
                 if (sock < 0)
                 {
@@ -79,7 +80,7 @@ static void udp_client_task(void *pvParameters)
                 }
                 ESP_LOGI(TAG, "Socket created");
                 static struct sockaddr_in mysock = {0};
-                mysock.sin_addr.s_addr = LocalIP->addr; //以wifi得到的ip初始化udp
+                mysock.sin_addr.s_addr = LocalIP->addr; //以wifi得到的ip  初始化udp
                 mysock.sin_family = AF_INET;
                 mysock.sin_port = htons(PORT);
                 int err = bind(sock, (struct sockaddr *)&mysock, sizeof(mysock));
@@ -87,7 +88,7 @@ static void udp_client_task(void *pvParameters)
                 {
                         ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
                 }
-                while (handel != NULL)
+                while (state)
                 {
                         static struct sockaddr_in reciveAddr; // Large enough for both IPv4 or IPv6
                         socklen_t socklen = sizeof(reciveAddr);
@@ -106,7 +107,6 @@ static void udp_client_task(void *pvParameters)
                                 event_t.len = len;
                                 event_t.recdata = rx_buffer;
                                 callback(&event_t);
-
                                 udp_client_sendto(reciveAddr.sin_addr.s_addr, "get mes");
                         }
                         vTaskDelay(200 / portTICK_PERIOD_MS);
@@ -125,15 +125,14 @@ static void udp_client_task(void *pvParameters)
 extern void udp_client_start(udp_callback_t call)
 {
         callback = call;
+        state = true;
         xTaskCreate(udp_client_task, "udp_client", 1024 * 8, NULL, 5, &handel);
 }
 
 extern void udp_client_stop()
 {
-        if (handel != NULL)
+        if (state)
         {
-                shutdown(sock, 0);
-                close(sock);
-                vTaskDelete(handel);
+                state = 0;
         }
 }
